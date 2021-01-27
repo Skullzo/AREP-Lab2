6a80ef13f64ef95923ff50940aff42d6e44f2435
Alejandro Toro Daza <skullzo@outlook.com>
2021-01-27 16:47:36 -0500

    Código Fuente

--- src/main/java/edu/escuelaing/arep/app/App.java
+++ src/main/java/edu/escuelaing/arep/app/App.java
@@ -1,8 +1,9 @@
 package edu.escuelaing.arep.app;
 import edu.escuelaing.arep.app.calculos.Calculadora;
 import edu.escuelaing.arep.app.listas.ListaEnlazada;
-import java.io.BufferedReader;
-import java.io.FileReader;
+import spark.Request;
+import spark.Response;
+import static spark.Spark.*;
 import java.text.DecimalFormat;
 /**
  * Clase encargada de llevar a cabo la ejecucion de todo el programa.
@@ -15,33 +16,93 @@ public class App {
      * @param args Parametro que indica la lista de los elementos a evaluar.
      */
     public static void main( String[] args ) {
-        try {
-        	ListaEnlazada<Double> elementos;
-            String sCadena;
-            BufferedReader bf = new BufferedReader(new FileReader("Valores.txt"));
-            double n;
-            while ((sCadena = bf.readLine())!=null) {
-                elementos = new ListaEnlazada<Double>();
-                String[] lista = sCadena.split(" ");
-                for (String elemento: lista){
-                    n = Double.parseDouble(elemento);
-                    elementos.add(n);
-                }
-                calcular(elementos);
-            }
-            bf.close();
-        }catch (Exception  e){
-            System.out.println("Se ha presentado un Error.");
-        }
+    	port(getPort());
+        get("/", (req, res) -> inputDataPage(req, res));
+        get("/results", (req, res) -> resultsPage(req, res));
+    }
+    private static String inputDataPage(Request req, Response res) {
+        String pageContent
+                = "<!DOCTYPE html>"
+                + "<html>"
+                + "<title> Calculadora</title>"
+                + "<body>"
+                + "<h1>Calculadora de la media y desviación estándar</h1>"
+                + "<form action=\"/results\">"
+                + "Por favor ingrese los datos separados por una coma(\",\"):<br>"
+                + "<input type=\"text\" name=\"Datos\">"
+                + "<br><br>"
+                + "<input type=\"submit\" value=\"Submit\">"
+                + "</form>"
+                + "<p>If you click the \"Submit\" button, the form-data will be sent to a page called \"/results\".</p>"
+                + "</body>"
+                + "</html>";
+        return pageContent;
     }
     /**
      * Metodo encargado de obtener la media y la desviacion estandar (obtenida a traves de la varianza) de los datos ingresados.
      * @param elementos Parametro que indica los elementos de la lista.
      */
-    public static void calcular(ListaEnlazada<Double> elementos){
-        DecimalFormat twoDForm = new DecimalFormat("#.##");
-        double media = Calculadora.media(elementos);
-        double desviacionEstandar = Calculadora.desviacionEstandar(elementos);
-        System.out.println("Media: " + twoDForm.format(media) + ", DesviaciÃ³n EstÃ¡ndar: "+ twoDForm.format(desviacionEstandar));
-    }
+    
+    /**
+    *
+    * @param req Tiene la informacion de la petición.
+    * @param res Tiene la información con la respuesta del servidor.
+    * @return pageContent Contiene la pagina html.
+    */
+   
+
+   /**
+    *
+    * @param req Tiene la informacion de la petición.
+    * @param res Tiene la información con la respuesta del servidor.
+    * @return pageResponse Contiene la pagina html.
+    */
+   private static String resultsPage(Request req, Response res) {
+       ListaEnlazada<Double> elementos = new ListaEnlazada<Double>();
+       String data = req.queryParams("Datos");
+       String[] lista  = data.split(",");
+       double n;
+       for (String elemento: lista){
+           n = Double.parseDouble(elemento);
+           elementos.add(n);
+       }
+       String[] calculos = calcular(elementos).split(" ");
+       String pageResponse
+               = "<!DOCTYPE html>"
+               + "<html>"
+               + "<title> Calculadora</title>"
+               + "<body>"
+               + "<h1>Resultados de las operaciones:</h1>\n"
+               + "El cálculo de la media de los datos ingresados es: "
+               + calculos[0]
+               + "<br>"
+               + "El cálculo de la desviación estándar de los datos ingresados es: "
+               + calculos[1]
+               + "<br>"
+               + "<a href=\"/\">Volver</a>"
+               + "</body>"
+               + "</html>";
+       return pageResponse;
+   }
+   public static String calcular(ListaEnlazada<Double> elementos){
+       DecimalFormat twoDForm = new DecimalFormat("#.##");
+       double media = Calculadora.media(elementos);
+       double desviacionEstandar = Calculadora.desviacionEstandar(elementos);
+       return twoDForm.format(media)+" "+twoDForm.format(desviacionEstandar);
+   }
+
+   /**
+    * This method reads the default port as specified by the PORT variable in
+    * the environment.
+    *
+    * Heroku provides the port automatically so you need this to run the
+    * project on Heroku.
+    * @return returns default port if heroku-port isn't set (i.e. on localhost)
+    */
+   static int getPort() {
+       if (System.getenv("PORT") != null) {
+           return Integer.parseInt(System.getenv("PORT"));
+       }
+       return 4567; //returns default port if heroku-port isn't set (i.e. on localhost)
+   }
 }
