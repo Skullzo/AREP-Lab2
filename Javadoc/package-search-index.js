7bc26df91569be4e134b5c4ddc9f9ea042fce16c
Alejandro Toro Daza <skullzo@outlook.com>
2021-01-27 15:06:54 -0500

    Documentación Borrada

--- Javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"edu.escuelaing.arep.app"},{"l":"edu.escuelaing.arep.app.calculos"},{"l":"edu.escuelaing.arep.app.listas"}];updateSearchResults();
