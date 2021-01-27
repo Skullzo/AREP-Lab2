8dd406dc639d11473c5da48c960bbc1242ab34e8
Alejandro Toro Daza <skullzo@outlook.com>
2021-01-27 18:26:52 -0500

    codigo

--- src/main/java/edu/escuelaing/arep/app/App.java
+++ src/main/java/edu/escuelaing/arep/app/App.java
@@ -40,7 +40,7 @@ public class App {
                 = "<!DOCTYPE html>"
                 + "<html>"
                 + "<title>Calculadora de Media y Desviacion Estandar</title>"
-                + "<body style = \"background: url(https://blog.hdwallsource.com/wp-content/uploads/2017/05/desk-calculator-wallpaper-61252-63066-hd-wallpapers.jpg) no-repeat ; background-size: 100% 105%;\">"
+                + "<body style = \"background: url(https://blog.hdwallsource.com/wp-content/uploads/2017/05/desk-calculator-wallpaper-61252-63066-hd-wallpapers.jpg) no-repeat ; background-size: 100% 110%;\">"
                 + "</body>"
                 + "<body>"
                 + "<div style=\"padding:30px; width:96.9%; background:#008000; color:white; font-size:500%; position:absolute ; top:0px ; left:0px\">"
